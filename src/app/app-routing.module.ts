257414c625e28514241f433f52aa490b30f36b81
phongtiger <phongken94@gmail.com>
2019-11-01 22:05:48 +0700

    tai len

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {Routes, RouterModule, PreloadAllModules} from '@angular/router';
+import {YoutubePlayerComponent} from './youtube-player/youtube-player.component';
+import {YoutubePlaylistComponent} from './youtube-playlist/youtube-playlist.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'youtube',
+    component: YoutubePlaylistComponent,
+    children: [{
+      path: ':id',
+      component: YoutubePlayerComponent
+    }]
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,
+    {
+      preloadingStrategy: PreloadAllModules
+    }
+  )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
